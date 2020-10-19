f337e0abaa10d07ac454d2f80d2cf27f31e2b535
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-10-19 13:10:53 +0200

    class to function component

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
-class Ninjas extends Component {
+
+const Ninjas = ({ ninjas }) => {
+  //const { ninjas } = props;
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>belt: {ninja.belt}</div>
+        <br />
+
+      </div>
+    )
+  })
+  return (
+    <div className="ninja-list">
+      {ninjaList}
+
+    </div>
+  )
+
+
+}
+/* class Ninjas extends Component {
   render() {
     const { ninjas } = this.props;
     const ninjaList = ninjas.map(ninja => {
@@ -21,6 +44,6 @@ class Ninjas extends Component {
       </div>
     )
   }
-}
+} */
 
 export default Ninjas;
