d4f01d698dc0abcfa12107c4daec6c88946c4b3f
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-10-26 18:58:15 +0200

    form 2

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ class App extends Component {
       { name: 'mohamed', age: '65', belt: 'red', id: 3 }
     ]
   }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
 
     return (
       <div className="App">
-        <h1>My first React app</h1>
+        <h1>React app</h1>
         <p>Welcome:)</p>
         <Ninjas ninjas={this.state.ninjas} />
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     )
   }
